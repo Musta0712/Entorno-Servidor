b57a2f386d77ac2171ba540d70b8ee1acd373952
Musta0712 <adrimustatea@gmail.com>
2025-11-11 11:07:06 +0100

    INCLUDES

---
+++ POO_ADRIANM/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Ataque.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/AtaqueEspecial.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/AtaqueFisico.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Generacion.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Pokemon.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/PokemonInicial.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/PokemonLegendario.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/PokemonSalvaje.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Tipo.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Usuario.php';
+
