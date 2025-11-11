aa2b8e6f7329f26a674616b42edb11f9c12c442c
Musta0712 <adrimustatea@gmail.com>
2025-11-11 11:14:52 +0100

    .

--- POO_ADRIANM/public/index.php
+++ POO_ADRIANM/public/index.php
@@ -1,13 +1,16 @@
 <?php
 
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Ataque.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/AtaqueEspecial.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/AtaqueFisico.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Generacion.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Pokemon.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/PokemonInicial.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/PokemonLegendario.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/PokemonSalvaje.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Tipo.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/app/models/Usuario.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Ataque.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/AtaqueEspecial.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/AtaqueFisico.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Generacion.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Pokemon.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonInicial.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonLegendario.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonSalvaje.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Tipo.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Usuario.php';
+
+$fuego = new Tipo("Fuego", ["Agua", "Roca"], ["Planta", "Hielo"]);
+$planta = new Tipo("Planta", ["Fuego", "Hielo"], ["Agua", "Roca"]);
 
