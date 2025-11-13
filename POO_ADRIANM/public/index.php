4aae1b640e95848062ee679d19dc06b396eccd10
Musta0712 <adrimustatea@gmail.com>
2025-11-13 13:54:22 +0100

    acabado

--- POO_ADRIANM/public/index.php
+++ POO_ADRIANM/public/index.php
@@ -1,15 +1,15 @@
 <?php
 
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Ataque.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/AtaqueEspecial.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/AtaqueFisico.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Generacion.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Pokemon.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonInicial.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonLegendario.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonSalvaje.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Tipo.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Usuario.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Ataque.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/AtaqueEspecial.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/AtaqueFisico.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Generacion.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Pokemon.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonInicial.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonLegendario.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/PokemonSalvaje.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Tipo.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/POO_ADRIANM/app/models/Usuario.php';
 
 ?>
 
