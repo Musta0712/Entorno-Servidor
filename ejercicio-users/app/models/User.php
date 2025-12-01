34c9ce0273aefea4b3aa08f9d35a9d866c5dedb9
Musta0712 <adrimustatea@gmail.com>
2025-12-01 10:48:04 +0100

    .

--- ejercicio-users/app/models/User.php
+++ ejercicio-users/app/models/User.php
@@ -1,5 +1,5 @@
 <?php
-include_once $_SERVER["DOCUMENT_ROOT"] . "/app/models/Region.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/ejercicios-users/app/models/Region.php";
 class User
 {
     public function __construct(
--- ejercicio-users/resources/views/layouts/header.php
+++ ejercicio-users/resources/views/layouts/header.php
@@ -3,9 +3,9 @@
     <h1>Mi aplicación web</h1>
     <nav>
         <ul class="nav-menu">
-            <li><a href="/public/form-login.php">Login</a></li>
-            <li><a href="/public/form-signup.php">Signup</a></li>
-            <li><a href="/public/index.php">Home</a></li>
+            <li><a href="/ejercicios-users/public/form-login.php">Login</a></li>
+            <li><a href="/ejercicios-users/public/form-signup.php">Signup</a></li>
+            <li><a href="/ejercicios-users/public/index.php">Home</a></li>
         </ul>
     </nav>
 </header>
