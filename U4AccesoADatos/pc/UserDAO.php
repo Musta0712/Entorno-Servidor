90a54309b8f43c0cf1fdca401df8fe68b39bc4f8
Musta0712 <adrimustatea@gmail.com>
2026-01-13 10:33:42 +0100

    .

--- U4AccesoADatos/pc/UserDAO.php
+++ U4AccesoADatos/pc/UserDAO.php
@@ -1,50 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    require_once $_SERVER["DOCUMENT_ROOT"] . "/U4AccesoADatos/pc/PcDAO.php";
-    require_once $_SERVER["DOCUMENT_ROOT"] . "/U4AccesoADatos/pc/UserDAO.php";
+<?php
 
-    $pc = new Pc("asus1997", "andrea", "Asus", 1364.1);
-    $c1 = new Component("ssd", "samsung", "58H");
-    $c2 = new Component("ram", "samsung", "W526");
-    $c3 = new Component("mouse", "logitech", "asd");
-    $pc->addComponent($c1);
-    $pc->addComponent($c2);
-    $pc->addComponent($c3);
+require_once $_SERVER["DOCUMENT_ROOT"] . "/U4AccesoADatos/pc/User.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/U4AccesoADatos/pc/CoreDB.php";
 
-    //Lo añado a la BD:
-    //PcDAO::create($pc);
+class UserDAO
+{
 
-    //echo PcDAO::read("asus125");
+    /**
+     * Inserta un usuario hasheando su contraseña
+     * @param User $user usuario a insertar, tiene la contraseña clara en sus parámetros
+     * @return int con el id del usuario insertado
+     */
+    public static function create($user)
+    {
+        //conexión
+        $conn = CoreDB::getConnection();
 
-    $u = new User("sete", "admin123987!!!---");
-    $u2 = new User("diego", "a");
-    $u3 = new User("marco", "a");
+        //sentencia preparada
+        $sql = "INSERT into users (name, password) values (?, ?)";
+        $ps = $conn->prepare($sql);
 
-    //Guardos los users en la BD:
-    //UserDAO::create($u);
-    //UserDAO::create($u2);
-    //UserDAO::create($u3);
+        //bind (pass la tenemos que hashear)
+        $name = $user->getName();
+        $pass = $user->getPass();   //$pass contiene la contraseña clara
+        $passHash = password_hash($pass, PASSWORD_DEFAULT); //$passHash tiene la contraseña hasheada
+        $ps->bind_param("ss", $name, $passHash);
 
+        //lanza
+        $ps->execute();
 
-    var_dump(UserDAO::verifyPassword("asdf", "asdf"));  //-1
-    var_dump(UserDAO::verifyPassword("sete", "asdf"));  //-2
-    var_dump(UserDAO::verifyPassword("sete", "admin123987!!!---")); //1
+        //recupera id
+        $id = $ps->insert_id;
+        $user->setId($id);
 
+        //cierra conexión
+        $conn->close();
 
-    ///
-    if (PcDAO::create($pc)){
-        echo "se ha creado :)";
-    }else{
-        echo "no se ha creado :(";
+        return $id;
     }
 
-    ?>
-</body>
-</html>
+    /**
+     * Verifica si una contraseña corresponde con la contraseña de eses nombre en la bd
+     * @param mixed $name nombre del usuario
+     * @param mixed $pass contraseña introducida que será verificada con la que está guardada
+     * en la bd
+     * @return int 1 si coinciden, -1 sin o existe el user, -2 si existe el user pero la contraseña
+     * no es correcta
+     */
+    public static function verifyPassword($name, $pass): int
+    {
+        $ret = 0;
+        $conn = CoreDB::getConnection();
+        $sql = "SELECT * from users where name = ?";
+        $ps = $conn->prepare($sql);
+        $ps->bind_param("s", $name);
+        $ps->execute();
+        $result = $ps->get_result();
+        $row = $result->fetch_assoc();
+        if ($row != null) {
+            $passBD = $row["password"];
+            if (password_verify($pass, $passBD)) {
+                $ret = 1;   //user y contraseña correctas
+            } else {
+                $ret = -2;  //user existe, pero su contraseña no es correcta
+            }
+        } else {
+            $ret = -1;  //el select no ha devuelto ningún resultado, por tanto no existe el user
+        }
+        $conn->close();
+        return $ret;
+    }
+}
